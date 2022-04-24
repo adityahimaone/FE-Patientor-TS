043ee3e4b9e93d5fd0d8a0e941a5564bd7a1d730
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-04-24 14:28:57 +0700

    Add Patient Page

--- src/pages/Patients.tsx
+++ src/pages/Patients.tsx
@@ -2,10 +2,18 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Patient } from "../types";
+import { useStateValue, setPatientList } from "../state";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
 
 export default function Patients() {
+  const [{ patients }, dispatch] = useStateValue();
   const { id } = useParams<{ id: string }>();
-  const [patientsData, setPatientsData] = useState([]);
 
   useEffect(() => {
     const fetchPatientList = async () => {
@@ -13,6 +21,7 @@ export default function Patients() {
         const { data: patientList } = await axios.get<Patient[]>(
           `http://localhost:3001/patients`
         );
+        dispatch(setPatientList(patientList));
       } catch (error) {
         console.error(error);
       }
@@ -25,7 +34,34 @@ export default function Patients() {
       <div>
         <h4>Patients List</h4>
       </div>
-      <div></div>
+      <div>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>SSN</TableCell>
+                <TableCell>Occupation</TableCell>
+                <TableCell>Gender</TableCell>
+                <TableCell>Detail</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.values(patients).map((patient: Patient) => (
+                <TableRow key={patient.id}>
+                  <TableCell>{patient.name}</TableCell>
+                  <TableCell>{patient.ssn}</TableCell>
+                  <TableCell>{patient.occupation}</TableCell>
+                  <TableCell>{patient.gender}</TableCell>
+                  <TableCell>
+                    <Button variant="contained">Detail</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 }
